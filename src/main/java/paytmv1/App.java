ba0b6563c4bd77ab19554e507946cb94efe019ca
sai <sai@devopsbyrushi.com>
2021-11-12 17:13:01 +0530

    paytm sale code

---
+++ src/main/java/paytmv1/App.java
@@ -0,0 +1,13 @@
+package paytmv1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello india!" );
+    }
+}
